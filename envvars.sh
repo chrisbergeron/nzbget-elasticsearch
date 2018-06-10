19f3468ecf47b54704cc04e71b17636541bedfa0
Chris Bergeron <cbergeron@gmail.com>
2018-06-10 19:04:10 -0400

    Updated optional debugging envvars.sh script.

--- envvars.sh
+++ envvars.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# This script is optional and can be used for testing.  Update the variables below for your environment then copy and paste the output into your terminal.
+# Now you can run ./ESLog.py  (if it's set executable)
+
 echo export NZBOP_NZBLOG=true
 echo export NZBPO_HOST="elk-01"
 echo export NZBPO_PORT=9200
